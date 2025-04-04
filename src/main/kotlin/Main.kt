7376b336bfda98ce39ff01be50af0620edd350c8
Mousaber00 <mousaber00@gmail.com>
2025-04-04 20:57:26 +0200

    Add test driven development naming to the cases

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,12 @@ fun main () {
 
 }
 
-fun addressChecker(addrress: String, dot: Char = '.'): Boolean {
+fun addressChecker(address: String, dot: Char = '.'): Boolean {
     val result = mutableListOf<String>()
     var segment = StringBuilder()
 
-    // Check  If The Address Is Not Reserved
-    if (addrress == "0.0.0.0") return false
-// Splits The Address Into Segments
-    for (number in addrress) {
+    // Splits The Address Into Segments
+    for (number in address) {
         if (number != '.' && number.isDigit().not()) return false
         if (number == dot) {
             result.add(segment.toString())
@@ -19,13 +17,16 @@ fun addressChecker(addrress: String, dot: Char = '.'): Boolean {
             segment.append(number)
         }
     }
-// Add The Last Item In The Segment That Is Not Followed by dot
+    // Add The Last Item In The Segment That Is Not Followed by dot
+
     result.add(segment.toString())
-// Loops The result Of Appending Segments Together
-    for (adress in result) {
+
+    // Loops The result Of Appending Segments Together
+
+    for (item in result) {
         if (result.size != 4) return false
-        if (adress.toInt() !in 0..255) return false
-        if (adress.startsWith("0") && adress != "0") return false
+        if (item.toInt() !in 0..255) return false
+        if (item.startsWith("0") && item != "0") return false
     }
     return true
 }
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -5,52 +5,57 @@ fun main() {
     //Valid Address
 
     test(
-        title = "When Address Is Valid",
+        title = "When Address Is Valid ,Should Return True",
         result = addressChecker("17.2.33.0"),
+        expectedResult = true,
+    )
+    test(
+        title = "When Address Contains A Reserved address 0.0.0.0,Should Return True",
+        result = addressChecker("0.0.0.0"),
         expectedResult = true,
     )
 
     //Invalid Address
 
     test(
-        title = "When Address Contains A Segment Larger Than 255",
+        title = "When Address Contains A Segment Larger Than 255,Should Return False",//
         result = addressChecker("17.22.444.6"),
         expectedResult = false,
     )
 
     test(
-        title = "When Address Contains A Segment Less Than Zero",
+        title = "When Address Contains A Segment Less Than Zero,Should Return False",
         result = addressChecker("2.-2.2.2"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains A Segment Leading With Zero",
+        title = "When Address Contains A Segment with Leading Zero,Should Return False",
         result = addressChecker("03.2.6.4"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains More Than 3 Dots",
+        title = "When Address Contains More Than 3 Dots,Should Return False",
         result = addressChecker("192.22.33.30.44)"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains Less Than 3 Dots",
+        title = "When Address Contains Less Than 3 Dots,Should Return False",
         result = addressChecker("23.23.23"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains A Different Spliter ",
+        title = "When Address Contains A Different Spliter,Should Return False",
         result = addressChecker("1,2.5.34"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains A Character",
+        title = "When Address Contains A Character,Should Return False",
         result = addressChecker("1.2.A.34"),
         expectedResult = false,
     )
     test(
-        title = "When Address Contains A Reserved Segments 0.0.0.0",
-        result = addressChecker("0.0.0.0"),
+        title = "When Address Is Empty,Should Return False",
+        result = addressChecker(""),
         expectedResult = false,
     )
 }
